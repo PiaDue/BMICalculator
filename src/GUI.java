e449510188d411f9428430170ae25075dd4e8949
PiaDue <pia.duenow@web.de>
2022-08-05 20:24:09 +0200

    added ActionListener

--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
     private JFrame frame;
     private JPanel panel;
+    private JButton button;
     private JTextField heightInput;
     private JTextField weightInput;
     private JLabel bmi;
@@ -13,7 +16,7 @@ public class GUI {
         frame = new JFrame();
         panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(10,10,20,10));
-        panel.setLayout(new GridLayout(5,2));
+        panel.setLayout(new GridLayout(6,2));
 
 
         JLabel heading = new JLabel("insert your measures");
@@ -23,21 +26,26 @@ public class GUI {
         JLabel weightLabel = new JLabel("weight: ");
         weightInput = new JTextField();
 
+        button = new JButton("Calculate");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bmi.setText("test");
+                health.setText("test");
+            }
+        });
+
         JLabel bmiLabel = new JLabel("BMI: ");
         bmi = new JLabel("");
         JLabel healtLabel = new JLabel("health: ");
         health = new JLabel("");
 
-        panel.add(heading);
-        panel.add(new Container());
-        panel.add(heightLabel);
-        panel.add(heightInput);
-        panel.add(weightLabel);
-        panel.add(weightInput);
-        panel.add(bmiLabel);
-        panel.add(bmi);
-        panel.add(healtLabel);
-        panel.add(health);
+        panel.add(heading);     panel.add(new Container());
+        panel.add(heightLabel); panel.add(heightInput);
+        panel.add(weightLabel); panel.add(weightInput);
+        panel.add(button);      panel.add(new Container());
+        panel.add(bmiLabel);    panel.add(bmi);
+        panel.add(healtLabel);  panel.add(health);
 
         frame.setBounds(400, 300, 100, 300);
         frame.add(panel);
