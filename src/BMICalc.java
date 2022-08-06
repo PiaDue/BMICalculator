a1c75803cadcefe3a8421f75f6a45cb254143343
PiaDue <pia.duenow@web.de>
2022-08-06 12:58:37 +0200

    safe changes

--- src/BMICalc.java
+++ src/BMICalc.java
@@ -1,2 +1,15 @@
 public class BMICalc {
+    private int weight;
+    private double height;
+    private int bmi;
+
+    public BMICalc(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+        this.bmi = weight/(height*height);
+    }
+
+    public int getBmi() {
+        return bmi;
+    }
 }
